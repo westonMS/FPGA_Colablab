31df203abc520394d6302a6bb60d99755e86d669
westonMS <97260484+westonMS@users.noreply.github.com>
2022-07-21 10:50:49 -0600

    Update add_8.sv

---
+++ Arithmetic/files/add_8.sv
@@ -0,0 +1,10 @@
+module 8add(
+  input logic Cin,
+  input logic [7:0] a, b,
+  output logic [7:0] s,
+  output logic Cout 
+  );
+  //Instantiate 8 instances of full_adder
+  //Have Cin be the first carry in and Cout be the last carry out
+  
+ endmodule
