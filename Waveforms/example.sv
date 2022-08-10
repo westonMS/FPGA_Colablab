b267fcc8131dc4291b99744d2d53e350d7404aac
Zach227 <zadriskill@gmail.com>
2022-08-10 09:42:47 -0600

    I updated the example design for waveforms

--- Waveforms/example.sv
+++ Waveforms/example.sv
@@ -1,11 +1,13 @@
+`timescale 1ns / 1ps
+
 module example(
     input logic inc,
     input logic clk,
     input logic reset,
     output logic[15:0] count
-)
+);
 
-always_ff(@posedge clk)
+always_ff @(posedge clk)
     if(reset)
         count <= 0;
     else if(inc)
