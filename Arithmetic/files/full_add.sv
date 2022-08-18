d608c9e13bbeea58b69eaca7c5cc972a4f7c54f7
westonMS <97260484+westonMS@users.noreply.github.com>
2022-08-18 10:56:29 -0600

    Update full_add.sv

--- Arithmetic/files/full_add.sv
+++ Arithmetic/files/full_add.sv
@@ -1,6 +1,6 @@
 module full_add(
-  input logic a, b, Cin,
-  output logic s, Cout
+  input logic a, b, cin,
+  output logic s, co
   );
-  //A, B and the Cin should be added together. s is the result and Cout signifies carry over.
+  //A, B and the cin should be added together. s is the result and co signifies carry over.
   endmodule
