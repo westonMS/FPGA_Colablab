31bd7e6d846020720eacb2143f17d08565c06d8b
westonMS <97260484+westonMS@users.noreply.github.com>
2022-10-22 15:15:51 -0600

    Update dataflow_sv.sv

--- bin/dataflow_sv.sv
+++ bin/dataflow_sv.sv
@@ -1,4 +1,4 @@
-module top(
+module dataflow_sv(
     input logic[15:0] sw,
     input wire logic btnu, btnd, btnl, btnr,
     output logic[15:0] led
@@ -6,20 +6,17 @@ module top(
 
 always_comb begin
     if(btnl) begin
-        led[15:8] = 0;
-        led[7:0] = sw[15:8] ^ sw[7:0];
+//Function 1
         end
     else if(btnr) begin
-        led[15:1] = 0;
-        if(sw == 16'hFFFF)
-            led[0] = 1'b1;
-        else
-            led[0] = 1'b0; 
+//Function 2
         end
-    else if(btnd)
-        led = sw << 3;
-    else if(btnu)
-        led = 0;
+    else if(btnd) begin
+//Function 3
+    end
+    else if(btnu) begin
+//Function 4
+    end
     else
         led = sw;
 end
