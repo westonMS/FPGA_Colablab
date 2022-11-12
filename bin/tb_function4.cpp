5c6855b5452609f9a9554e4bdd318dd98ec40080
westonMS <97260484+westonMS@users.noreply.github.com>
2022-11-12 15:32:14 -0700

    Update tb_function4.cpp

--- bin/tb_function4.cpp
+++ bin/tb_function4.cpp
@@ -146,23 +146,15 @@ void check_sw(Vfunction4 *dut, vluint64_t sim_time)
 int main(int argc, char **argv, char **env)
 {
     Vfunction4 *dut = new Vfunction4;
-
     Verilated::traceEverOn(true);
-    VerilatedVcdC *m_trace = new VerilatedVcdC;
-    dut->trace(m_trace, 5);
-    m_trace->open("waveform.vcd");
-
     while (sim_time < MAX_SIM_TIME)
     {
         change_sw(dut, sim_time);
         check_sw(dut, sim_time);
         // dut->clk ^= 1;
         dut->eval();
-        m_trace->dump(sim_time);
         sim_time++;
     }
-
-    m_trace->close();
     delete dut;
     exit(EXIT_SUCCESS);
 }
